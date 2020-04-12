d3f8913b33c8018ef9c4b0030312f0fb2fe14c93
chrisnager <cnager@gmail.com>
2020-04-11 20:28:58 -0400

    Simplify home hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,19 +1,13 @@
-import React from 'react'
-import Img from 'gatsby-image'
+/** @jsx jsx */
 
-import styles from './hero.module.css'
+import { jsx, Box, Text } from 'theme-ui'
 
 export default ({ data }) => (
-  <div className={styles.hero}>
-    <Img
-      className={styles.heroImage}
-      alt={data.name}
-      fluid={data.heroImage.fluid}
-    />
-    <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{data.name}</h3>
-      <p className={styles.heroTitle}>{data.title}</p>
-      <p>{data.shortBio.shortBio}</p>
-    </div>
-  </div>
+  <Box>
+    <Text as="h1">{data.name}</Text>
+    <Text as="p">
+      <Text as="b">{data.title}</Text>
+    </Text>
+    <Text as="p">{data.shortBio.shortBio}</Text>
+  </Box>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,22 @@
+/** @jsx jsx */
+
+import { jsx, Text } from 'theme-ui'
 import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
 
 class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
     const [author] = get(this, 'props.data.allContentfulPerson.edges')
 
     return (
       <Layout location={this.props.location}>
-        <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
-          <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        </div>
+        <Helmet title={siteTitle} />
+        <Hero data={author.node} />
       </Layout>
     )
   }
@@ -64,9 +51,7 @@ export const pageQuery = graphql`
         }
       }
     }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
+    allContentfulPerson(filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }) {
       edges {
         node {
           name
@@ -75,12 +60,7 @@ export const pageQuery = graphql`
           }
           title
           heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-              background: "rgb:000000"
-            ) {
+            fluid(maxWidth: 1180, maxHeight: 480, resizingBehavior: PAD, background: "rgb:000000") {
               ...GatsbyContentfulFluid_tracedSVG
             }
           }
