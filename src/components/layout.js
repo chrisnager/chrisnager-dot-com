bbe5f19053daefb1e2b2aa8ccda795ee62760d63
chrisnager <cnager@gmail.com>
2020-04-12 20:24:10 -0400

    Change favicon based on color mode

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,13 +3,22 @@
 import './index.css'
 
 import { jsx, Box } from 'theme-ui'
-
+import Helmet from 'react-helmet'
 import Navigation from './navigation'
 import Social from './social'
 
 const Layout = ({ children }) => {
   return (
     <Box sx={{ maxWidth: `75ch`, mx: `auto` }}>
+      <Helmet>
+        <link rel="icon" media="(prefers-color-scheme:dark)" href="favicon-dark.ico" type="image/x-icon" />
+        <link rel="icon" media="(prefers-color-scheme:light)" href="favicon.ico" type="image/x-icon" />
+        <script
+          src="https://unpkg.com/favicon-switcher@1.2.2/dist/index.js"
+          crossorigin="anonymous"
+          type="application/javascript"
+        ></script>
+      </Helmet>
       <Navigation />
       {children}
       <Social />
