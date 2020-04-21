81c8ef08572cccd2d59135a71ba0ab2bb6ff1fae
chrisnager <cnager@gmail.com>
2020-04-21 15:45:39 -0400

    Add projects and tags, add featured section

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -17,7 +17,37 @@ export default ({ data }) => {
 
         <Box as="ul" sx={{ my: 0, pl: 0, display: `grid` }}>
           {data.allSitesYaml.edges
-            .filter(({ node }) => node.page === `projects`)
+            .filter(({ node }) => node.page === `projects` && node.featured)
+            .map(({ node }) => (
+              <Flex
+                key={node.name}
+                as="a"
+                href={node.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                sx={{ mt: 4, ':hover': { textDecoration: `none` } }}
+              >
+                <Box sx={{ size: 120, mr: 2, flex: `0 0 120px`, bg: `action` }}>
+                  {node.childScreenshot && (
+                    <Img fluid={node.childScreenshot.screenshotFile.childImageSharp.fluid} alt={node.name} />
+                  )}
+                </Box>
+                <Box>
+                  <Text as="h1" sx={{ fontSize: 4, 'a:hover > &': { textDecoration: `underline` } }}>
+                    {node.name}
+                  </Text>
+                  <Text as="p" sx={{ color: `text`, 'a:hover > &': { color: `text` } }}>
+                    {node.summary}
+                  </Text>
+                  <Text as="p" sx={{ color: `text`, 'a:hover > &': { color: `text` } }}>
+                    {node.tags}
+                  </Text>
+                </Box>
+              </Flex>
+            ))}
+
+          {data.allSitesYaml.edges
+            .filter(({ node }) => node.page === `projects` && !node.featured)
             .map(({ node }) => (
               <Flex
                 key={node.name}
@@ -29,10 +59,7 @@ export default ({ data }) => {
               >
                 <Box sx={{ size: 80, mr: 2, flex: `0 0 80px`, bg: `action` }}>
                   {node.childScreenshot && (
-                    <Img
-                      resolutions={node.childScreenshot.screenshotFile.childImageSharp.resolutions}
-                      alt={node.name}
-                    />
+                    <Img fluid={node.childScreenshot.screenshotFile.childImageSharp.fluid} alt={node.name} />
                   )}
                 </Box>
                 <Box>
@@ -41,6 +68,9 @@ export default ({ data }) => {
                   </Text>
                   <Text as="p" sx={{ color: `text`, 'a:hover > &': { color: `text` } }}>
                     {node.summary}
+                  </Text>
+                  <Text as="p" sx={{ color: `text`, 'a:hover > &': { color: `text` } }}>
+                    {node.tags}
                   </Text>
                 </Box>
               </Flex>
@@ -60,11 +90,13 @@ export const pageQuery = graphql`
           name
           summary
           page
+          tags
+          featured
           childScreenshot {
             screenshotFile {
               childImageSharp {
-                resolutions(width: 80, height: 80) {
-                  ...GatsbyImageSharpResolutions
+                fluid(maxWidth: 240, maxHeight: 240) {
+                  ...GatsbyImageSharpFluid
                 }
               }
             }
