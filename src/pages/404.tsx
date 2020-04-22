cc5ef307c758ffaff29707d411c58eaef90f6db6
chrisnager <cnager@gmail.com>
2020-04-22 18:37:37 -0400

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+
+import { Link } from 'gatsby'
+import { FC } from 'react'
+import Helmet from 'react-helmet'
+import { Box, jsx } from 'theme-ui'
+
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+
+const NotFound: FC = () => {
+  return (
+    <Layout>
+      <Helmet title="Page not found / Chris Nager" />
+      <Box sx={{ maxWidth: `55ch`, mb: 5, px: 3 }}>
+        <Intro title="Page not found" description="The page you're looking for has been removed or relocated." />
+        Go <Link to="/">home</Link>.
+      </Box>
+    </Layout>
+  )
+}
+
+export default NotFound
