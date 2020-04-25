174abf4d7ef42ed63d315612391ab6a94619f88e
chrisnager <cnager@gmail.com>
2020-04-25 15:32:13 -0400

    Add share card image, update page meta tags

--- src/components/halo.tsx
+++ src/components/halo.tsx
@@ -8,7 +8,7 @@ const base = {
   title: `Chris Nager`,
   url: `https://chrisnager.com`,
   description: `Developer and designer in Brooklyn, NY passionate about performance, accessibility, and systematic design.`,
-  image: `logo.png`,
+  image: `https://chrisnager.com/chris-nager-card.png`,
   author: `@chrisnager`,
   hasCodePenEmbed: false,
 }
@@ -38,11 +38,14 @@ const Halo: FC<HaloProps> = ({
 
     {/* Facebook */}
     <meta property="og:url" content={url} />
+    <meta property="og:site_name" content="Chris Nager" />
     <meta property="og:type" content="article" />
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
     <meta property="og:image" content={image} />
-    {/* 1200 x 628px */}
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="628" />
+    <meta property="og:image:alt" content={`"Chris Nager" displayed in black on a light blue background`} />
 
     {/* Twitter */}
     <meta name="twitter:card" content="summary_large_image" />
@@ -50,7 +53,8 @@ const Halo: FC<HaloProps> = ({
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
     <meta name="twitter:image" content={image} />
-    {/* 1200 x 628px */}
+    <meta name="twitter:site" content={author} />
+    <meta name="twitter:creator" content={author} />
 
     {/* Icons */}
     {/* TODO: Make content hexcode match dynamic background color */}
@@ -64,10 +68,12 @@ const Halo: FC<HaloProps> = ({
     <link rel="manifest" href="manifest.json" />
 
     {/* Legacy */}
+
     {/* <meta charset="utf-8" /> */}
     {/* <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> */}
     {/* <meta name="viewport" content="width=device-width" /> */}
     {/* <meta property="fb:admins" content="41803347" /> */}
+    {/* <meta property="fb:app_id" content="" /> */}
     {/* <meta name="twitter:site" content="@chrisnager" /> */}
     {/* <meta name="twitter:url" content="http://chrisnager.com" /> */}
     {/* <meta name="application-name" content="chrisnager.com" /> */}
@@ -78,6 +84,8 @@ const Halo: FC<HaloProps> = ({
     {/* <link rel="shortcut icon" href="http://chrisnager.com/img/logo.png" /> */}
     {/* <link rel="icon" type="image/png" href="http://chrisnager.com/img/logo.png" /> */}
     {/* <link rel="apple-touch-icon" href="http://chrisnager.com/img/logo.png" /> */}
+    {/* <meta name="apple-mobile-web-app-title" content="short-title" /> */}
+    {/* <meta name="application-name" content="short-title" /> */}
 
     {hasCodePenEmbed && <script async src="https://static.codepen.io/assets/embed/ei.js"></script>}
   </Helmet>
