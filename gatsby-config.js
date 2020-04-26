2c5471e87f3d53500c48cc4f17068deb013e2dd1
chrisnager <cnager@gmail.com>
2020-04-26 05:14:01 -0400

    Add no-javascript plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     // `gatsby-plugin-mdx`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-no-javascript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: { path: `./src/data/` },
